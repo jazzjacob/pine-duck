df5c8e4f9c4c5801137b391b96abdf319dea39e6
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2021-09-30 13:25:37 +0200

    Add files via upload

---
+++ simple-card-game/card-game-script.js
@@ -0,0 +1,206 @@
+import {convertCardToNumberValue} from './functions.js';
+
+const deckButton = document.querySelector('#deck-button');
+const resetButton = document.querySelector('#reset-button');
+const buttonOne = document.querySelector('#button-one');
+const buttonTwo = document.querySelector('#button-two');
+const playerOneCard = document.querySelector('.player-one > .card');
+const playerTwoCard = document.querySelector('.player-two > .card');
+const deck = document.querySelector('#deck');
+const p1scoreElement = document.querySelector('.player-one .score');
+const p2scoreElement = document.querySelector('.player-two .score');
+const startGameElement = document.querySelector('#start-game');
+const main = document.querySelector('main');
+
+
+const newDeckURL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+
+let remainingCards;
+let deckID;
+let p1DrawnCard;
+let p2DrawnCard;
+let p1score = 0;
+let p2score = 0;
+
+const nextRoundButton = document.createElement('button');
+
+deckButton.addEventListener('click', async function(event) {
+  const response = await fetch(newDeckURL);
+  const newDeck = await response.json();
+  deckID = newDeck.deck_id;
+  remainingCards = newDeck.remaining;
+  let deckMessage = `Cards remaining in deck: `;
+  const p = document.createElement('p');
+  p.innerText = deckMessage;
+  const counter = document.createElement('span');
+  counter.className = 'remaining-cards';
+  counter.innerText = remainingCards;
+  p.appendChild(counter);
+  deck.appendChild(p);
+  
+  if (deckID) {
+    buttonOne.classList.remove('hidden');
+    buttonTwo.classList.remove('hidden');
+    resetButton.classList.remove('remove');
+    startGameElement.style.display = 'none';
+  }
+
+})
+
+buttonOne.addEventListener('click', async function(event) {
+  if (deckID) {
+    const drawCardURL = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`;
+    const response = await fetch(drawCardURL);
+    const drawnCard = await response.json();
+    const cardImage = drawnCard.cards[0].images.png;
+    
+    playerOneCard.style.backgroundImage = `url(${cardImage})`;
+    playerOneCard.style.borderRadius = 0;
+    playerOneCard.style.backgroundColor = 'white';
+    
+    p1DrawnCard = drawnCard.cards[0].code;
+    p1DrawnCard = convertCardToNumberValue(p1DrawnCard);
+    
+    remainingCards = drawnCard.remaining;
+    
+    const counter = document.querySelector('.remaining-cards');
+    counter.innerText = remainingCards;
+    
+    if (p2DrawnCard) {
+      if (p1DrawnCard > p2DrawnCard) {
+        buttonOne.innerText = 'Winner!';
+        buttonOne.classList.add('winner');
+        
+        buttonTwo.innerText = 'Loser';
+        p1score++;
+        p1scoreElement.innerText = p1score;
+      } else {
+        buttonTwo.innerText = 'Winner!';
+        buttonTwo.classList.add('winner');
+        buttonOne.innerText = 'Loser';
+        p2score++;
+        p2scoreElement.innerText = p2score;
+      }
+      if (nextRoundButton.style.display === 'none') {
+        nextRoundButton.style.display = 'block';
+      }
+      buttonOne.disabled = true;
+      buttonTwo.disabled = true;
+      nextRoundButton.innerText = 'Next round';
+      main.appendChild(nextRoundButton);
+      p1DrawnCard = null;
+      p2DrawnCard = null;
+    }
+  }
+})
+
+
+buttonTwo.addEventListener('click', async function(event) {
+  if (deckID) {
+    const drawCardURL = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`;
+    const response = await fetch(drawCardURL);
+    const drawnCard = await response.json();
+    const cardImage = drawnCard.cards[0].images.png;
+    
+    playerTwoCard.style.backgroundImage = `url(${cardImage})`;
+    playerTwoCard.style.borderRadius = 0;
+    playerTwoCard.style.backgroundColor = 'white';
+    
+    p2DrawnCard = drawnCard.cards[0].code;
+    p2DrawnCard = convertCardToNumberValue(p2DrawnCard);
+    
+    remainingCards = drawnCard.remaining;
+    
+    const counter = document.querySelector('.remaining-cards');
+    counter.innerText = remainingCards;
+        
+    if (p1DrawnCard) {
+      if (p1DrawnCard > p2DrawnCard) {
+        buttonOne.innerText = 'Winner!';
+        buttonOne.classList.add('winner');
+        
+        buttonTwo.innerText = 'Loser';
+        p1score++;
+        p1scoreElement.innerText = p1score;
+      } else {
+        buttonTwo.innerText = 'Winner!';
+        buttonTwo.classList.add('winner');
+        buttonOne.innerText = 'Loser';
+        p2score++;
+        p2scoreElement.innerText = p2score;
+      }
+      if (nextRoundButton.style.display === 'none') {
+        nextRoundButton.style.display = 'block';
+      }
+      nextRoundButton.innerText = 'Next round';
+      main.appendChild(nextRoundButton);
+      buttonOne.disabled = true;
+      buttonTwo.disabled = true;
+      p1DrawnCard = null;
+      p2DrawnCard = null;
+    }
+  }
+})
+
+nextRoundButton.addEventListener('click', function(event) {
+  if (buttonOne.classList.value.includes('winner')) {
+    buttonOne.classList.remove('winner');
+  } else if (buttonTwo.classList.value.includes('winner')) {
+    buttonTwo.classList.remove('winner');
+  }
+  buttonOne.innerText = 'Draw card';
+  buttonTwo.innerText = 'Draw card';
+  
+  playerOneCard.style.backgroundImage = null;
+  playerTwoCard.style.backgroundImage = null;
+  playerOneCard.style.backgroundColor = 'gold';
+  playerTwoCard.style.backgroundColor = 'gold';
+  playerOneCard.style.borderRadius = '10px';
+  playerTwoCard.style.borderRadius = '10px';
+  
+  nextRoundButton.style.display = 'none';
+  
+  buttonOne.disabled = false;
+  buttonTwo.disabled = false;
+})
+
+resetButton.addEventListener('click', async function(event) {
+  const reshuffleDeckURL = `https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`;
+  const response = await fetch(reshuffleDeckURL);
+  const reshuffledDeck = await response.json();
+  remainingCards = reshuffledDeck.remaining;
+  const counter = document.querySelector('.remaining-cards');
+  counter.innerText = remainingCards;
+  
+  p1DrawnCard = null;
+  p2DrawnCard = null;
+  p1score = 0;
+  p2score = 0;
+  p1scoreElement.innerText = p1score;
+  p2scoreElement.innerText = p1score;
+  
+  if (buttonOne.classList.value.includes('winner')) {
+    buttonOne.classList.remove('winner');
+  } else if (buttonTwo.classList.value.includes('winner')) {
+    buttonTwo.classList.remove('winner');
+  }
+  buttonOne.innerText = 'Draw card';
+  buttonTwo.innerText = 'Draw card';
+  console.dir(buttonOne.classList);
+  
+  playerOneCard.style.backgroundImage = null;
+  playerTwoCard.style.backgroundImage = null;
+  playerOneCard.style.backgroundColor = 'gold';
+  playerTwoCard.style.backgroundColor = 'gold';
+  playerOneCard.style.borderRadius = '10px';
+  playerTwoCard.style.borderRadius = '10px';
+  
+  nextRoundButton.style.display = 'none';
+  
+  if (buttonOne.disabled) {
+    buttonOne.disabled = false;
+    buttonTwo.disabled = false;
+  }
+})
+
+
---
+++ simple-card-game/functions.js
@@ -0,0 +1,68 @@
+export const convertCardToNumberValue = (cardCode) => {
+  switch (cardCode) {
+    case '2C': return 1;
+    case '2D': return 2;
+    case '2H': return 3;
+    case '2S': return 4;
+    
+    case '3C': return 5;
+    case '3D': return 6;
+    case '3H': return 7;
+    case '3S': return 8;
+    
+    case '4C': return 9;
+    case '4D': return 10;
+    case '4H': return 11;
+    case '4S': return 12;
+    
+    case '5C': return 13;
+    case '5D': return 14;
+    case '5H': return 15;
+    case '5S': return 16;
+    
+    case '6C': return 17;
+    case '6D': return 18;
+    case '6H': return 19;
+    case '6S': return 20;
+    
+    case '7C': return 21;
+    case '7D': return 22;
+    case '7H': return 23;
+    case '7S': return 24;
+    
+    case '8C': return 25;
+    case '8D': return 26;
+    case '8H': return 27;
+    case '8S': return 28;
+    
+    case '9C': return 29;
+    case '9D': return 30;
+    case '9H': return 31;
+    case '9S': return 32;
+    
+    case '0C': return 33;
+    case '0D': return 34;
+    case '0H': return 35;
+    case '0S': return 36;
+    
+    case 'JC': return 37;
+    case 'JD': return 38;
+    case 'JH': return 39;
+    case 'JS': return 40;
+    
+    case 'QC': return 41;
+    case 'QD': return 42;
+    case 'QH': return 43;
+    case 'QS': return 44;
+    
+    case 'KC': return 45;
+    case 'KD': return 46;
+    case 'KH': return 47;
+    case 'KS': return 48;
+    
+    case 'AC': return 49;
+    case 'AD': return 50;
+    case 'AH': return 51;
+    case 'AS': return 52;
+  }
+}
