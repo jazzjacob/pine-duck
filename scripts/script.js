bd43c84e5257583349a01b430c5c59dc819e46e4
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2021-10-12 22:46:36 +0200

    added info on index, more on snow

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,10 @@
 const linksList = document.querySelector('.links-nav ul');
 const links = document.querySelectorAll('.links-nav a');
-console.dir(links);
 
 linksList.addEventListener('mouseover', event => {
-  console.dir(event.target.localName);
   const typeOfTargetElement = event.target.localName;
   const targetElement = event.target;
   if (typeOfTargetElement === "a") {
-    console.log('yes');
     event.target.classList.add('links-hover-box');
   }
 })
